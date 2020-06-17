368ba094643bb320f1b166961667d99aca9663c1
sujan <sujanpoojary884@gmail.com>
2020-06-17 14:20:07 +0530

    node types and cloning a node

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
-/* query selector */
-/* returns only one element, that matches the criteria */
-const bookName = document.querySelector('#book-list li:nth-child(2)');
 
-// console.log(bookName);
 
-/* 
-    querySelectorAll returns the Nodelist through we can applay all array methods
+const banner = document.querySelector('#page-banner');
+
+console.log("#page-banner Node type: ", banner.nodeType);
+console.log("#page-banner Node Name: ", banner.nodeName);
+
+console.log("is #page-banner has child node", banner.hasChildNodes());
+
+const bannerCopy = banner.cloneNode(true);
+console.log(bannerCopy);
+
+/*
+    'true' - helps to create a deep copy by coping all it's child node as well
+    'false' - inverse of true
 */
-const bookList = document.querySelectorAll('#book-list li .name');
-console.log(bookList);
 
-bookList.forEach(book => {
-    // console.log(book);
-    //console.log(book.textContent)
-    console.log(book.innerHTML);
-})
 
